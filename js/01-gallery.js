5626e61080f0f5f9e41408ba08cee6bd30b1367e
Criss9005 <cdjpn5@gmail.com>
2023-10-23 17:52:42 -0500

    second commit

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,49 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery')
+
+galleryItems.forEach(element => {
+    const item = document.createElement('li');
+    gallery.append(item);
+    item.classList.add('gallery__item')
+    const link = document.createElement('a');
+    item.append(link);
+    link.classList.add('gallery__link');
+    link.href = element.original;
+    const image = document.createElement('img');
+    image.classList.add('gallery__image');
+    link.append(image);
+    image.src = element.preview
+    image.alt = element.description
+    image.data = element.original
+
+});
+
+
+
+gallery.addEventListener('click', (e) => { 
+    e.preventDefault()
+    
+    galleryItems.forEach(element => {
+        if (element.description == e.target.alt) { 
+            
+            const instance = basicLightbox.create(`<img src="${element.original}">`)
+            instance.show()
+
+           
+            gallery.addEventListener('keydown', (e) => {
+                if (e.keyCode == 27) { 
+                    console.log('cerro')
+                    instance.close()
+                }
+                
+            })
+        }
+    });
+    
+    
+})
+
+
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,30 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery')
+
+galleryItems.forEach(element => {
+    const item = document.createElement('li');
+    gallery.append(item);
+    item.classList.add('gallery__item')
+    const link = document.createElement('a');
+    item.append(link);
+    link.classList.add('gallery__link');
+    link.href = element.original;
+    const image = document.createElement('img');
+    image.classList.add('gallery__image');
+    link.append(image);
+    image.src = element.preview
+    image.alt = element.description
+    image.title = element.description
+    
+});
+
+const options = {
+        
+        captionDelay: 300,
+        captionPosition: 'bottom',
+};
+
+const galleryShow = new SimpleLightbox('.gallery li a', {...options });
+
